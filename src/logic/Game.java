ad34de0c721cf403b47e7971cf0c2a32788c7cdd
MyCoffeeNeedsMoreJava <71220495+MyCoffeeNeedsMoreJava@users.noreply.github.com>
2021-07-04 10:38:24 +0200

    Added enums for Players. Added methods for switching playerInTurn()

--- src/logic/Game.java
+++ src/logic/Game.java
@@ -1,4 +1,53 @@
 package logic;
 
+import java.util.*;
+
+import logic.Dice;
+import player.Player;
+
+import static player.Player.*;
+
 public class Game {
+
+    private int hit;
+    private Player player;
+    private Player inTurn;
+
+    public void main() {
+        inTurn = PLAYER_BLUE;
+
+    }
+
+    public Player playerInTurn() {
+        return player;
+    }
+
+    public void endTurn() {
+        nextPlayer();
+    }
+
+    //Method to find player for next turn
+    public Player nextPlayer() {
+        switch (inTurn) {
+            case PLAYER_BLUE:
+                inTurn = PLAYER_RED;
+                break;
+            case PLAYER_RED:
+                inTurn = PLAYER_BLACK;
+                break;
+            case PLAYER_BLACK:
+                inTurn = PLAYER_GREEN;
+                break;
+            case PLAYER_GREEN:
+                inTurn = PLAYER_ORANGE;
+                break;
+            case PLAYER_ORANGE:
+                inTurn = PLAYER_YELLOW;
+                break;
+            case PLAYER_YELLOW:
+                inTurn = PLAYER_BLUE;
+                break;
+        }
+        return inTurn;
+    }
 }
---
+++ src/player/Player.java
@@ -0,0 +1,10 @@
+package player;
+
+public enum Player {
+    PLAYER_BLUE,
+    PLAYER_RED,
+    PLAYER_GREEN,
+    PLAYER_BLACK,
+    PLAYER_ORANGE,
+    PLAYER_YELLOW;
+}
